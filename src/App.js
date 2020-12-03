e55ab5c9c1822b4de74ed2145fbfdfae39243d89
Rob <robrichardsdpt@gmail.com>
2020-12-03 12:05:32 -0500

    created form input, sign in, and button components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component.jsx'
 import Header from './components/header/header.component.jsx'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
+import SignInandSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path ='/' component={HomePage}/>
           <Route path ='/shop' component={ShopPage}/>
+          <Route path='/signin' component={SignInandSignUpPage}/>
         </Switch>
     </div>
   )
