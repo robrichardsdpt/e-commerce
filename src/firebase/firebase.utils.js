f65d80d3339312815db9148ee2ef14115b4cbd85
Rob <robrichardsdpt@gmail.com>
2020-12-04 11:56:07 -0500

    firestore for google sign in updated.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,32 @@ const config = {
     measurementId: "G-NZ2VC3F5EC"
   }
 
-firebase.initializeApp(config)
+  firebase.initializeApp(config)
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return
+      const userRef = firestore.doc(`users/${userAuth.uid}`)
+      const snapShot = await userRef.get()
+      console.log(snapShot)
+      if(!snapShot.exists) {
+          const { displayName, email } = userAuth
+          const createdAt = new Date()
+
+          try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+          } catch(error){
+              console.log('error creating user', error.message)
+          }
+      }
+      return userRef
+  }
+
+
 
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
