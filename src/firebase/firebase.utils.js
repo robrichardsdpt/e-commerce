a6cb97832bb753f3e45e7c621f0f7c6888d6d340
Rob <robrichardsdpt@gmail.com>
2020-12-04 15:33:34 -0500

    finished sign in component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -18,7 +18,6 @@ const config = {
     if(!userAuth) return
       const userRef = firestore.doc(`users/${userAuth.uid}`)
       const snapShot = await userRef.get()
-      console.log(snapShot)
       if(!snapShot.exists) {
           const { displayName, email } = userAuth
           const createdAt = new Date()
