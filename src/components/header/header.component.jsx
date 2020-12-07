5c08c3d2d1453c96287264e0786ec3d0834e8cd7
Rob <robrichardsdpt@gmail.com>
2020-12-07 11:03:49 -0500

    created cart icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component'
 import './header.styles.scss'
 
 const Header = ({ currentUser }) => (
@@ -19,6 +20,7 @@ const Header = ({ currentUser }) => (
             :
             <Link className='option' to='/signin'>SIGN IN</Link>
         }
+        <CartIcon/>
         </div>
     </div>
 )
