8f54277c3a563ad779d1992e5e91a165dc29dec3
Rob <robrichardsdpt@gmail.com>
2020-12-07 16:52:57 -0500

    added cart icon, cart dropdown, and cart reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 // combines all of our states together in one large json obj
 import { combineReducers } from 'redux'
 import userReducer from './user/user.reducer'
+import cartReducer from './cart/cart.reducer'
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 })
