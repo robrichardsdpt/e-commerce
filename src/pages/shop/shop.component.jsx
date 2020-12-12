3ce98b14912ec969fdd37eec8d2248476fd697fc
Rob <robrichardsdpt@gmail.com>
2020-12-12 16:43:06 -0500

    collection completed

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import CollectionOverview from '../../components/collections-overview/collections-overview.component'
+import CollectionPage from '../collection/collection.component'
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
     <div className='shop-page'>
-              <CollectionOverview/> 
-        </div>
+            <Route exact path={`${match.path}`} component={CollectionOverview}/> 
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
 )
-
 export default ShopPage
