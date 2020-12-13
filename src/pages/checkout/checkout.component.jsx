d960ad889944d112f90bfa385bc8148bfee05494
Rob <robrichardsdpt@gmail.com>
2020-12-13 08:58:37 -0500

    project fully functional.  next step optimization.

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import CheckoutItem from './../../components/checkout-item/checkout-item.component'
-
+import StripeCheckoutButton from './../../components/stripe/stripe-button.component'
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 
 import './checkout.styles.scss'
@@ -30,8 +30,12 @@ const CheckoutPage = ({ cartItems, total }) => (
             cartItems.map(cartItem => (<CheckoutItem key={cartItem.id} cartItem={cartItem}/>)
             )}
         <div className='total'>
-        <span>TOTAL = {total}</span>
+        <span>TOTAL = {total}</span></div>
+        <div className = 'test-warning'>
+            *Please use the following test credit card for payments* <br/>
+            4242 4242 4242 4242 - exp: 12/21 - CVV: 123
         </div>
+        <StripeCheckoutButton price={total}/>
     </div>
 )
 
