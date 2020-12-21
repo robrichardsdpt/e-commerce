f2ec0863f33f147663e0c00f4c9c244df9cab811
Rob <robrichardsdpt@gmail.com>
2020-12-20 19:34:10 -0500

    added redux thunk and async functionality

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -18,4 +18,14 @@ export const selectCollection = memoize((collectionUrlParam) => (
         [selectCollections],
         collections => collections ? collections[collectionUrlParam] : null)
     )
-    )
+  )
+
+  export const selectIsCollectionFetching = createSelector(
+      [selectShop],
+      shop => shop.isFetching
+  )
+
+  export const selectIsCollectionsLoaded = createSelector(
+      [selectShop],
+      shop => !!shop.collections
+  )
